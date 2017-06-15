997bcbfa4e8d95f06840988ca1497d02e337ce9d
Chris Bay <chris@chrisbay.net>
2017-06-15 09:09:30 -0500

    Add salting

--- hashutils.py
+++ hashutils.py
@@ -1,12 +1,22 @@
 import hashlib
+import random
+import string
 
 
-def make_pw_hash(password):
-    return hashlib.sha256(str.encode(password)).hexdigest()
+def make_salt():
+    return ''.join([random.choice(string.ascii_letters) for x in range(5)])
+
+
+def make_pw_hash(password, salt=None):
+    if not salt:
+        salt = make_salt()
+    hash = hashlib.sha256(str.encode(password + salt)).hexdigest()
+    return '{0},{1}'.format(hash, salt)
 
 
 def check_pw_hash(password, hash):
-    if make_pw_hash(password) == hash:
+    salt = hash.split(',')[1]
+    if make_pw_hash(password, salt) == hash:
         return True
 
     return False
